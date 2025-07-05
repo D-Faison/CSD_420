8e4e03cf1d131e56d1fb6cdf10ca79b84542b8c2
D-Faison <dfaison2017@gmail.com>
2025-07-05 15:29:34 -0400

    Update

--- module-7/faison_m7/src/main/java/myapp/App.java
+++ module-7/faison_m7/src/main/java/myapp/App.java
@@ -3,6 +3,8 @@ package myapp;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 /*DeJanae Faison
@@ -18,9 +20,29 @@ import javafx.stage.Stage;
         //Set Hbox with set spacing
         HBox hbox = new HBox(5);
         //Scene with hbox as root
-        Scene scene = new Scene(hbox,500,450);
+        Scene scene = new Scene(hbox,300,200);
+        //Attach stylesheet
+        scene.getStylesheets().add("mystyle.css");
 
+        //Add circles and their resepctive panes
+        Pane pane1 = new Pane();
+        Circle circle1 = new Circle(50,100,30);
+        pane1.getChildren().addAll(circle1);
+        pane1.getStyleClass().add("border");
+        circle1.getStyleClass().add("plaincircle");
 
+        Pane pane2 = new Pane();
+        Circle circle2 = new Circle(50,100,30);
+        pane2.getChildren().addAll(circle2);
+        circle2.getStyleClass().add("plaincircle");
+
+        Pane pane3 = new Pane();
+        Circle circle3 = new Circle(50,100,30);
+        pane3.getChildren().addAll(circle3);
+        circle3.setId("redcircle");
+
+        //Add Panes to hbox
+        hbox.getChildren().addAll(pane1,pane2,pane3);
         //set the stage
         primaryStage.setTitle("Faison M7 CSS Java");
         primaryStage.setScene(scene);
