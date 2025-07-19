c8f3daaced3c6582529241ac4fa938eabae5b929
D-Faison <dfaison2017@gmail.com>
2025-07-19 15:30:55 -0400

    Update Display

--- module-10/FaisonFansDB/src/Faison10Fan.java
+++ module-10/FaisonFansDB/src/Faison10Fan.java
@@ -1,6 +1,5 @@
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 /*DeJanae Faison
  * M10: View and Update fan database
@@ -13,6 +12,7 @@ import java.sql.DriverManager;
 public class Faison10Fan{
 
     //Connector Variable
+    @SuppressWarnings("unused")
     Connection con;
 
     @SuppressWarnings("UseSpecificCatch")
@@ -34,14 +34,26 @@ public class Faison10Fan{
         }catch(Exception e){
             System.err.println("!!!!Error to connect to database!!!");
         }
+    }
 
-        //Close the connection
+    public Records getFanById(int id) {
+        Records fan = null;
         try {
-            con.close();
-            System.out.println("==|Connection Closed|==");
-        } catch (Exception e) {
-            System.out.println("==!!!Connection Close Failed!!!==");
+            String query = "SELECT * FROM fans WHERE id = ?";
+            PreparedStatement preparedStatement = con.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                fan = new Records();
+                fan.setId(resultSet.getInt("id"));
+                fan.setFirstName(resultSet.getString("firstname"));
+                fan.setLastName(resultSet.getString("lastname"));
+                fan.setFavoriteTeam(resultSet.getString("favoriteTeam"));
+            }
+        } catch (SQLException e) {
+            System.err.println("Error fetching fan by ID: " + e.getMessage());
         }
+        return fan;
     }
 
     public static void main(String[] args) {
--- module-10/FaisonFansDB/src/GUI.java
+++ module-10/FaisonFansDB/src/GUI.java
@@ -3,7 +3,8 @@ import javax.swing.*;
 /* GUI For M10 */
 
 public class GUI extends JFrame {
-
+    //Database Variable
+    Faison10Fan db = new Faison10Fan();
     //UI Components: Labels
     static JLabel idLabel;
     static JLabel firstNameLabel;
@@ -29,7 +30,7 @@ public class GUI extends JFrame {
         setLayout(new GridLayout(6,2));
 
         //ID section
-        idLabel = new JLabel("ID:");
+        idLabel = new JLabel("Enter ID:");
         add(idLabel);
 
         idTextField = new JTextField();
@@ -58,6 +59,24 @@ public class GUI extends JFrame {
         add(displayButton);
         updateButton = new JButton("Update");
         add(updateButton);
+
+        //Action Listener for Display Button
+        displayButton.addActionListener(e -> {
+            try{
+                int id = Integer.parseInt(idTextField.getText().trim());
+                Records fan = db.getFanById(id);
+
+                if (fan != null) {
+                    firstNameTextField.setText(fan.getFirstName());
+                    lastNameTextField.setText(fan.getLastName());
+                    favoriteTeamTextField.setText(fan.getFavoriteTeam());
+                } else {
+                    JOptionPane.showMessageDialog(this, "Fan not found!", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(this, "Invalid ID format!", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        });
     }
 
     public static void main(String[] args) {
